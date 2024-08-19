4243db25d6a7028e734a85613bb779eb8e2c906d
katashafar <shafar032@gmail.com>
2024-08-20 00:33:21 +0700

    merubah beberapa konfigurasi lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {}
 
-export default nextConfig;
+export default nextConfig
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
